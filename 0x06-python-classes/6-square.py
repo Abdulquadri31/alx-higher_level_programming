06313a5616bce3bef57547a82aaacb46075e8acf
Abdulquadri31 <abdulquadriakinde@gmail.com>
2024-10-01 22:51:03 +0100

    class and obj

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -103,4 +103,4 @@ class Square:
         
         # Print the square with the specified position
         for _ in range(self.__size):
-            print(" " * self.__position[0] + "#" * self.__size)I
+            print(" " * self.__position[0] + "#" * self.__size)
